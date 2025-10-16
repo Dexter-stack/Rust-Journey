ca09576b4c9ec615486965c245b455448c2b6240
feelinzmvp <feelinzmvpmmp@gmail.com>
2025-10-16 11:15:56 +0100

    first exercise done function to convert from temperatures

---
+++ src/Exercise.rs
@@ -0,0 +1,20 @@
+// Convert temperatures between Fahrenheit and Celsius.
+
+fn conver_c_to_f(temp :i32)-> i32{
+    return (temp*9/5)+32;
+}
+
+fn convert_f_to_c(temp :i32)-> i32{
+    return (temp -32)*5/9;
+}
+
+
+fn main(){
+
+println!("The value of {}, from degree celcious to F is {}", 32, conver_c_to_f(32));
+println!("The value of {}, from degree F to C is {}", 32, convert_f_to_c(32));
+
+
+
+
+}
