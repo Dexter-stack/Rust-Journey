142f62bc7b4f324940d56d51785af823ae944978
feelinzmvp <feelinzmvpmmp@gmail.com>
2025-10-16 11:20:16 +0100

    fibonacci exercise done

--- src/Exercise.rs
+++ src/Exercise.rs
@@ -14,6 +14,22 @@ fn main(){
 println!("The value of {}, from degree celcious to F is {}", 32, conver_c_to_f(32));
 println!("The value of {}, from degree F to C is {}", 32, convert_f_to_c(32));
 
+println!("The value of {} fibonaci is {}", 9, fib(9));
+
+
+fn fib(num: i32)-> i32{
+
+    if num ==0{
+        return 0;
+    }
+
+    if num ==1{
+        return 1;
+    }
+
+    return fib(num-1)+fib(num-2);
+}
+
 
 
 
